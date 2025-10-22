d61f42c5eaa166ffeca9241e988e3623c6b48e62
Viwu <sinconsistencia@gmail.com>
2025-10-21 21:11:46 -0500

    CM

---
+++ command_manager.py
@@ -0,0 +1,12 @@
+commands = [
+    {
+        "name": "dummy-1",
+        "description": "Dummy command",
+        "function": print("Hello World! I am a dummy!")
+    },
+    {
+        "name": "dummy-2",
+        "description": "Dummy command Dos",
+        "function": print("Hello World! I am another dummy!")
+    }
+]
